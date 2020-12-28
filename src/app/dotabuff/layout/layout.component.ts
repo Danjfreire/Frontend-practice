3e5a7df98eb716235d2f1093d943a311c5d1338c
Daniel Freire <danjfreire@gmail.com>
2020-12-28 14:17:41 -0300

    feat: created initial module

---
+++ src/app/dotabuff/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dotabuff',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
