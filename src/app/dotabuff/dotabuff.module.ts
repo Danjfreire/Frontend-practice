f421cae32537974f240c970484583d555a980944
Daniel Freire <danjfreire@gmail.com>
2020-12-30 11:38:11 -0300

    feat: updated layout

--- src/app/dotabuff/dotabuff.module.ts
+++ src/app/dotabuff/dotabuff.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
+import { PostComponent } from './post/post.component';
 
 
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent],
+  declarations: [LayoutComponent, HeaderComponent, PostComponent],
   imports: [
-    CommonModule
+    CommonModule,
   ],
   exports: [
     LayoutComponent,
