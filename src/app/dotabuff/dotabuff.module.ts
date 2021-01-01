fd481d89a792094295489eda060c25dfd17d25d8
Daniel Freire <danjfreire@gmail.com>
2021-01-01 12:58:54 -0300

    chore: renamed activity1->player-activity

--- src/app/dotabuff/dotabuff.module.ts
+++ src/app/dotabuff/dotabuff.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { PostComponent } from './post/post.component';
-import { Activity1Component } from './activity1/activity1.component';
+import { PlayerActivity } from './player-activity/player-activity.component';
 
 
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, PostComponent, Activity1Component],
+  declarations: [LayoutComponent, HeaderComponent, PostComponent, PlayerActivity],
   imports: [
     CommonModule,
   ],
--- src/app/dotabuff/player-activity/player-activity.component.spec.ts
+++ src/app/dotabuff/player-activity/player-activity.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Activity1Component } from './activity1.component';
+import { PlayerActivity } from './player-activity.component';
 
-describe('Activity1Component', () => {
-  let component: Activity1Component;
-  let fixture: ComponentFixture<Activity1Component>;
+describe('PlayerActivityComponent', () => {
+  let component: PlayerActivity;
+  let fixture: ComponentFixture<PlayerActivity>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ Activity1Component ]
+      declarations: [ PlayerActivity ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(Activity1Component);
+    fixture = TestBed.createComponent(PlayerActivity);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
